c54b29c058411a19ba73f381567ceaddc306a888
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-24 18:18:35 +0900

    기본타입 - 문자열,숫자,배열 학습

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,15 @@
+//JS
+var str = 'hello';
+
+// TS
+
+// 문자열
+var str: string = 'hello';
+
+// 숫자
+let num: number = 10;
+
+// 배열
+let arr: Array<number> = [1, 2, 3];
+let heros: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
