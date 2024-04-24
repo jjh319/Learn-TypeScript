95b38a7d706a3325258a8e7296c7996efc4f0fb2
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-24 13:39:38 +0900

    js에 타입 정의하기

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,17 +9,56 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ * @
+ */
+
+/**
+ *
+ * @returns {Promise<User></User>}
+ */
+
+function fetchUser() {
+    return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+    response.address.city;
+});
+
 function startApp() {
-  axios
-    .get(url)
-    .then(function (response) {
-      console.log(response);
-      user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    // axios
+    //     .get(url)
+    fetchUser()
+        .then(function (response) {
+            // console.log(response);
+            user = response.data;
+            // TODO: 이름, 이메일, 주소 표시하기
+            console.log(user);
+            username.innerHTML = user[0].name;
+            email.innerHTML = user[0].email;
+            address.innerHTML =
+                user[0].address.street +
+                ', ' +
+                user[0].address.suite +
+                ', ' +
+                user[0].address.city +
+                ', ' +
+                user[0].address.zipcode;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 startApp();
