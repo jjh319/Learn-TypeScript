31198e08c533aed8de969ec5af874d95e7ddd1e6
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-24 14:02:30 +0900

    자바스크립트에 타입이 있을 때의 장점 학습

---
+++ why-ts/sample.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a + b;
+}
+
+// sum(10, '20'); // 1020
+var result2 = sum(10, 20);
+// result2.toLocaleString();
---
+++ why-ts/sample.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+    return a + b;
+} // sum
+
+var result = add(10, 20);
+result.toLocaleString(); // 숫자를 문자열로 변환
